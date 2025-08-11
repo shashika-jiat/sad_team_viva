197da74869fefe2242e6b774df6835773ebfe830
shashika-jiat <jar.shasika999@gmail.com>
2025-08-12 01:11:59 +0530

    add data model files to repository

---
+++ Pos_desktop/TeamBDesktop/src/DataModels/BrandItem.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataModels;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author Shasika
+ */
+public class BrandItem {
+    private int id;
+    private Image img = null;
+    private String itemCode = null;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Image getImg() {
+        return img;
+    }
+
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+    
+}
